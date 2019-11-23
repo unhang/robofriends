4230e50ea06ef0293ac133a1e63554c452b0e676
Nhat Han <nhathan@Han.local>
2019-11-23 15:48:17 +0700

    Updates

---
+++ precache-manifest.657a229f4856695f061c62af16df221d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb65dbc93bc804286ca905b91fa5ea3a",
+    "url": "/unhang/robofriends.git/index.html"
+  },
+  {
+    "revision": "115b80e92e4dd7d541ab",
+    "url": "/unhang/robofriends.git/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "208aee9a50e28970476f",
+    "url": "/unhang/robofriends.git/static/css/main.e8b8bc45.chunk.css"
+  },
+  {
+    "revision": "115b80e92e4dd7d541ab",
+    "url": "/unhang/robofriends.git/static/js/2.9a3f2569.chunk.js"
+  },
+  {
+    "revision": "208aee9a50e28970476f",
+    "url": "/unhang/robofriends.git/static/js/main.9dd4d062.chunk.js"
+  },
+  {
+    "revision": "a4a356de82eb54eac6ea",
+    "url": "/unhang/robofriends.git/static/js/runtime-main.09642336.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/unhang/robofriends.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
