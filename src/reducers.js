c0c3861099a6493b222c2f2b616f2838ff4e91d7
Ung Nhat Hang <hang.ung@executionlab.asia>
2021-03-15 15:00:29 +0700

    update package

--- src/reducers.js
+++ src/reducers.js
@@ -1,38 +1,47 @@
 import {
-    CHANGE_SEARCH_FILED,
-    REQUEST_ROBOTS_PENDING,
-    REQUEST_ROBOTS_SUCCESS,
-    REQUEST_ROBOTS_FAILED
-} from './constants';
+  CHANGE_SEARCH_FILED,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+} from "./constants";
 
 const intialStateSearch = {
-    searchfield: ''
-}
+  searchfield: ""
+};
 
 export const searchRobots = (state = intialStateSearch, action = {}) => {
-    switch (action.type) {
-        case CHANGE_SEARCH_FILED:
-            return Object.assign({}, state, { searchfield: action.payload });
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case CHANGE_SEARCH_FILED:
+      return Object.assign({}, state, { searchfield: action.payload });
+    default:
+      return state;
+  }
+};
 
 const initialStateRequestRobots = {
-    robots: [],
-    isPending: false,
-    error: ''
-}
+  robots: [],
+  isPending: false,
+  error: ""
+};
 
-export const requestRobots = (state = initialStateRequestRobots, action = {}) => {
-    switch (action.type) {
-        case REQUEST_ROBOTS_PENDING:
-            return Object.assign({}, state, { isPending: true });
-        case REQUEST_ROBOTS_SUCCESS:
-            return Object.assign({}, state, { robots: action.payload, isPending: false });
-        case REQUEST_ROBOTS_FAILED:
-            return Object.assign({}, state, { error: action.payload, isPending: false });
-        default:
-            return state;
-    }
-}
+export const requestRobots = (
+  state = initialStateRequestRobots,
+  action = {}
+) => {
+  switch (action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return Object.assign({}, state, { isPending: true });
+    case REQUEST_ROBOTS_SUCCESS:
+      return Object.assign({}, state, {
+        robots: action.payload,
+        isPending: false
+      });
+    case REQUEST_ROBOTS_FAILED:
+      return Object.assign({}, state, {
+        error: action.payload,
+        isPending: false
+      });
+    default:
+      return state;
+  }
+};
